38df38e50569804a1fed6fd056ba7b1b3ca3c8d1
Lytves <22356770+lytves@users.noreply.github.com>
2021-01-02 18:09:51 +0100

    Update config.py

--- my_private_bot/config.py
+++ my_private_bot/config.py
@@ -1,5 +1,5 @@
 # the token of your Bot
-TOKEN_BOT = ''
+TOKEN_BOT = 'put_your_bot_token_here'
 
 # Telegram chat/channel ID to redirect the messages
-YOUR_TELEGRAM_CHAT_ID = ''
+YOUR_TELEGRAM_CHAT_ID = 'put_your_chat_id_here'
